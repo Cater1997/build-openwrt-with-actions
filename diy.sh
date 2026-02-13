d3d58e61289fc106570c81e86649208202b8d370
Cater1997 <92653925+Cater1997@users.noreply.github.com>
2026-02-13 08:38:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,6 +5,9 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 
 # 编辑默认的主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/CONFIG_PACKAGE_kmod-fast-classifier=y/# CONFIG_PACKAGE_kmod-fast-classifier=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-shortcut-fe-cm=y/# CONFIG_PACKAGE_kmod-shortcut-fe-cm=y/' .config
+sed -i 's/CONFIG_PACKAGE_kmod-shortcut-fe-drv=y/# CONFIG_PACKAGE_kmod-shortcut-fe-drv=y/' .config
 
 # 编辑默认的luci显示的固件名称
 #sed -i 's/OpenWrt/ZWRT/g' package/base-files/files/bin/config_generate
